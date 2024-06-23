b6c5781c24d52692f14a3dc7c256c79c3a2dffd0
David <mercadodavidv@gmail.com>
2024-06-23 11:29:46 -0500

    Implement cell reveal flooding

--- src/useGrid.js
+++ src/useGrid.js
@@ -8,11 +8,11 @@ const surroundingDirections = [
   [-1, 1],  [0, 1],  [1, 1],
 ];
 
-function initializeCell(colIdx, rowIdx) {
-  return Object({ isRevealed: false, isMined: false, colIdx: colIdx, rowIdx: rowIdx, nearbyMineCount: 0 });
+function initializeCell(x, y) {
+  return Object({ isRevealed: false, isMined: false, colIdx: x, rowIdx: y, nearbyMineCount: 0 });
 }
 
-function initializeGrid(rows = 10, columns = 10, mines = 10) {
+function initializeGrid(columns = 10, rows = 10, mines = 10) {
   const grid = Array.from({ length: rows }).fill(Array.from({ length: columns })).map((row, rowIdx) => row.map((cell, colIdx) => initializeCell(colIdx, rowIdx)));
   for (let i = 0; i < mines; i++) {
     const randomX = randomInt(0, columns);
@@ -28,20 +28,42 @@ function initializeGrid(rows = 10, columns = 10, mines = 10) {
   return grid;
 }
 
-export function useGrid(rows = 10, columns = 10, mines = 10) {
-  const [grid, setGrid] = useState(initializeGrid(rows, columns, mines));
+export function useGrid(columns = 10, rows = 10, mines = 10) {
+  const [grid, setGrid] = useState(initializeGrid(columns, rows, mines));
 
-  const revealCell = useCallback((colIdx, rowIdx) => {
-    setGrid(currentGrid => {
+  const revealCell = useCallback((x, y) => {
+    setGrid(grid => {
+      // Recursively flood to adjacent cells.
+      const revealAdjacent = (x, y) => {
+        const currentCell = grid[y]?.[x];
+
+        // Base case.
+        if (currentCell?.nearbyMineCount > 0) {
+          return;
+        }
+
+        // Recursive case.
+        surroundingDirections.forEach(direction => {
+          const adjacentX = x + direction[0];
+          const adjacentY = y + direction[1];
+          const adjacentCell = grid[adjacentY]?.[adjacentX];
+          if (adjacentCell && !adjacentCell.isRevealed) {
+            grid[adjacentY][adjacentX].isRevealed = true;
+            revealAdjacent(adjacentX, adjacentY);
+          }
+        });
+      };
+      revealAdjacent(x, y);
+
       // Reveal the cell that was clicked.
-      currentGrid[rowIdx][colIdx].isRevealed = true;
+      grid[y][x].isRevealed = true;
 
-      return [...currentGrid];
+      return [...grid];
     });
   });
 
-  const resetGrid = useCallback((rows = 10, columns = 10, mines = 10) => {
-    setGrid(initializeGrid(rows, columns, mines));
+  const resetGrid = useCallback((columns = 10, rows = 10, mines = 10) => {
+    setGrid(initializeGrid(columns, rows, mines));
   });
 
   return [grid, revealCell, resetGrid];
