9d761f9fe5b81d9504c064867d13553acba5c519
David <mercadodavidv@gmail.com>
2024-06-25 07:09:05 -0500

    Format files

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <div className='position-relative user-select-none ms-grid-space'>
-        <div className='position-absolute start-50 top-50 translate-middle-x' style={{ display: 'grid', grid: `auto-flow / repeat(${grid.length}, auto)`, gap: '0.5rem'}}>
+        <div className='position-absolute start-50 top-50 translate-middle-x' style={{ display: 'grid', grid: `auto-flow / repeat(${grid.length}, auto)`, gap: '0.5rem' }}>
           {grid.map((row, rowIdx) =>
             row.map((cell, colIdx) =>
               <Cell key={rowIdx + colIdx} className='d-inline' cell={cell} revealCell={revealCell} flagCell={flagCell} />
